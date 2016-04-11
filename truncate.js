9729ca3bdfe9ca4665e0420b40068f15b3cc7eb3
Arianna Marin <marin.arianna@gmail.com>
2016-04-10 20:49:58 -0700

    initial todos completed

--- truncate.js
+++ truncate.js
@@ -1,23 +1,36 @@
 // This is the file where you will write the Truncate Words function and related code.
-
-// TODO: Create a Function called truncateWords() that accepts two arguments: 
-// longText (a String with several words in it)
-// numWords (an Integer that sets the number of words you want in the returned text)
-
-// TODO: Within the truncateWords() Function, complete the following steps:
-// 1. Use the split() function to split the String into an Array
-// 2. Use the length attribute to find the number of words in the Array
-// 3. Determine how many words should be removed from the String
-// 4. Remove those words from the Array
-// 5. Add an additional String item to the Array to put an ellipses in: "..."
-// 6. Use the join() function to convert the Array back into a String
-// 7. Return the truncated String from the Function
-
-// This portion of the script is meant to call and display the result of your Function.
-// You do not need to change the following lines, but you may change them if you 
-// are experimenting further or pursuing stretch goals.
 var originalText = "There are two hard things in Computer Science: Cach invalidation, naming things, and off-by-one errors.";
 var wordLimit = 8;
+// TODO: Create a Function called truncateWords() that accepts two arguments:
+// longText (a String with several words in it)
+// numWords (an Integer that sets the number of words you want in the returned text)
+var truncateWords = function(longText, numWords){
+    // 1. Use the split() function to split the String into an Array
+    var longTextArray = originalText.split (' ');
+    // 2. Use the length attribute to find the number of words in the Array
+    var numWords = longTextArray.length;
+    // 3. Determine how many words should be removed from the String
+    var shortTextArray = longTextArray.splice (0, numWords);
+    // 5. Add an additional String item to the Array to put an ellipses in: "..."
+    shortTextArray.push ('...');
+    // 6. Use the join() function to convert the Array back into a String
+    var shortText = shortTextArray.join (' ');
+    // 7. Return the truncated String from the Function
+    return shortText;
+}
+// TODO: Within the truncateWords() Function, complete the following steps:
+
+
+
+// 4. Remove those words from the Array
+
+
+
+
+// This portion of the script is meant to call and display the result of your Function.
+// You do not need to change the following lines, but you may change them if you
+// are experimenting further or pursuing stretch goals.
+
 var shortText = truncateWords(originalText, wordLimit);
 console.log('originalText: ' + originalText);
 console.log('shortText: ' + shortText);
